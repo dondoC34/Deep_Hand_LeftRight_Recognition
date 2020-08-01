9c3ffebc395ec353c985c7a48a29da261c0d71af
dondoC34 <dondo6135@tf-machine.us-central1-a.c.velvety-arc-283616.internal>
2020-08-01 15:17:58 +0000

    upload different models predictions from gcp

--- tf_predictor.py
+++ tf_predictor.py
@@ -1,6 +1,7 @@
 from tf_models import leNet
 from Data_Pipeline import data_generator
 import os
+import pandas as pd
 
 if __name__ == "__main__":
     conv_layers = [(50, 4, 4), (100, 3, 3), (150, 3, 3), (200, 3, 3), (300, 2, 2)]
@@ -21,10 +22,22 @@ if __name__ == "__main__":
                   input_shape=(256, 144, 1))
 
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy", "AUC"])
-    model.load_weights("Model_Weights/best_model_weights")
+    model.load_weights("Model_Weights/best_model_weights_loss")
 
     predictions = model.predict(x=streaming_pipeline_test,
                                 steps=int(NUM_OF_TEST_IMAGES / PIPELINE_BATCH_TEST))
 
-    print(predictions)
+    predictions_nr = len(predictions)
+    labels = []
+    for i in range(predictions_nr):
+        if i % 2 == 0:
+            labels.append(0)
+        else:
+            labels.append(1)
+    labeled_predictions = list(zip(predictions, labels))
+    
+    prediction_frame = pd.DataFrame(labeled_predictions, columns=["Prediction", "Class"])
+    prediction_frame.to_csv("Models_Predictions/loss_monitor_model_predictions.csv")
+
+    
 
