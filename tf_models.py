a3a3dbf1bd2a612a7b1b1825763804803dca245c
dondo34 <luca.dondoni@mail.polimi.it>
2020-08-09 16:55:40 +0200

    leNet set-up for hyper-parameters tuning - gcp

--- tf_models.py
+++ tf_models.py
@@ -14,7 +14,7 @@ def leNet(conv_layers, dense_layers, interpose_pooling_layers=False, interpose_d
                                      kernel_size=(conv_layers[0][1], conv_layers[0][2]),
                                      input_shape=input_shape))
     if interpose_pooling_layers:
-        model.add(tf.keras.layers.AvgPool2D(pool_size=(2, 2)))
+        model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 
     if interpose_dropout:
         model.add(tf.keras.layers.Dropout(rate=0.2))
@@ -23,11 +23,13 @@ def leNet(conv_layers, dense_layers, interpose_pooling_layers=False, interpose_d
         model.add(tf.keras.layers.Conv2D(filters=conv_layer[0],
                                          kernel_size=(conv_layer[1], conv_layer[2])))
         if interpose_pooling_layers:
-            model.add(tf.keras.layers.AvgPool2D(pool_size=(2, 2)))
+            model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 
     model.add(tf.keras.layers.Flatten())
     for dense_layer in dense_layers:
         model.add(tf.keras.layers.Dense(dense_layer, activation="relu"))
+
+    # output layer for binary classification
     model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
 
     return model
@@ -87,7 +89,7 @@ if __name__ == "__main__":
     first_dense_layers = [500, 600, 700]
     second_dense_layers = [500, 600, 700]
     conv_layers = [(50, 4, 4), (100, 3, 3), (150, 3, 3), (200, 3, 3), (300, 2, 2)]
-    dense_layers = [400, 500, 600, 700]
+    dense_layers = [400, 500, 600]
     PIPELINE_BATCH = 64
     PIPELINE_BATCH_TEST = 100
     TRAIN_FOLDER = "../ssd/Training_Frames/"
@@ -96,7 +98,6 @@ if __name__ == "__main__":
     NUM_OF_TEST_IMAGES = len(os.listdir(TEST_FOLDER + "Right/")) + len(os.listdir(TEST_FOLDER + "Left/"))
     EPOCHS = 200
     es = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True)
-    tensorboard = tf.keras.callbacks.TensorBoard(log_dir="Tensorboard_logs\leNet_esLoss_avgpool")
     aug = ImageDataGenerator(width_shift_range=0.05,
                              height_shift_range=0.05,
                              zoom_range=0.25,
@@ -110,42 +111,56 @@ if __name__ == "__main__":
     #                   interpose_pooling_layers=True,
     #                   input_shape=(256, 144, 1))
 
-    model = leNet(conv_layers=conv_layers,
-                  dense_layers=dense_layers,
-                  interpose_pooling_layers=True)
+    for i in range(10):
+        print("""
+              #################################################################################
+              #################################################################################
+              ITERATION WITH DENSE NR: {}
+              #################################################################################
+              #################################################################################
+              """.format(len(dense_layers) - i))
+        model = leNet(conv_layers=conv_layers,
+                      dense_layers=dense_layers,
+                      interpose_pooling_layers=True,
+                      input_shape=(256, 144, 1))
 
-    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy", "AUC"])
+        model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
-    streaming_pipeline_train = data_generator(input_folder=TRAIN_FOLDER,
-                                              data_augmentation=aug,
-                                              batch_size=PIPELINE_BATCH,
-                                              rescale=(144, 256),
-                                              mode="train")
-    streaming_pipeline_test = data_generator(input_folder=TEST_FOLDER,
-                                             data_augmentation=None,
-                                             batch_size=PIPELINE_BATCH_TEST,
-                                             rescale=(144, 256),
-                                             normalization_factor=1 / 255,
-                                             mode="eval")
-    history = model.fit(x=streaming_pipeline_train,
-                        steps_per_epoch=100,
-                        epochs=EPOCHS,
-                        validation_data=streaming_pipeline_test,
-                        validation_steps=int(NUM_OF_TEST_IMAGES / PIPELINE_BATCH_TEST),
-                        callbacks=[es, tensorboard])
+        streaming_pipeline_train = data_generator(input_folder=TRAIN_FOLDER,
+                                                  data_augmentation=aug,
+                                                  batch_size=PIPELINE_BATCH,
+                                                  rescale=(144, 256),
+                                                  mode="train")
+        streaming_pipeline_test = data_generator(input_folder=TEST_FOLDER,
+                                                 data_augmentation=None,
+                                                 batch_size=PIPELINE_BATCH_TEST,
+                                                 rescale=(144, 256),
+                                                 normalization_factor=1 / 255,
+                                                 mode="eval")
+        history = model.fit(x=streaming_pipeline_train,
+                            steps_per_epoch=100,
+                            epochs=EPOCHS,
+                            validation_data=streaming_pipeline_test,
+                            validation_steps=int(NUM_OF_TEST_IMAGES / PIPELINE_BATCH_TEST),
+                            callbacks=[es])
 
-    history_dict = history.history
-    loss = history_dict["loss"]
-    acc = history_dict["acc"]
-    val_loss = history_dict["val_loss"]
-    val_acc = history_dict["val_acc"]
-    frame_list = []
+        history_dict = history.history
+        loss = history_dict["loss"]
+        acc = history_dict["acc"]
+        val_loss = history_dict["val_loss"]
+        val_acc = history_dict["val_acc"]
+        frame_list = []
 
-    for i in range(len(loss)):
-        frame_list.append([x[i] for x in [loss, acc, val_loss, val_acc]])
-    frame = pd.DataFrame(frame_list, columns=["loss", "acc", "val-loss", "val-acc"])
-    frame.to_csv("Models_History/Hist_leNet_esLoss.csv_avgpool")
+        for i in range(len(loss)):
+            frame_list.append([x[i] for x in [loss, acc, val_loss, val_acc]])
+        frame = pd.DataFrame(frame_list, columns=["loss", "acc", "val-loss", "val-acc"])
+        frame.to_csv("Models_History/Hist_leNet_esLoss_" + str(i) + "_dense.csv")
 
-    model.save_weights("Model_Weights/We_leNet_esLoss_avgpool")
+        model.save_weights("Model_Weights/We_leNet_esLoss_" + str(i) + "_dense")
+        try:
+            dense_layers.pop(-1)
+        except IndexError:
+            break
+
 
 
